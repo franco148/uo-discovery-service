3ba372403f47c5356b3c690a4f38e6e088bd7930
franco-extreme <franck_ral@hotmail.com>
2018-05-02 22:02:34 -0400

    uo-discovery-service: Initial structure of the project.

---
+++ src/main/java/com/fral/uo/discovery/uodiscoveryservice/UoDiscoveryServiceApplication.java
@@ -0,0 +1,12 @@
+package com.fral.uo.discovery.uodiscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UoDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UoDiscoveryServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/fral/uo/discovery/uodiscoveryservice/UoDiscoveryServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fral.uo.discovery.uodiscoveryservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UoDiscoveryServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
